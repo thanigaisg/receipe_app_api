4f384b7fcb043fdfebce932d570807a9a5bc8719
Thanigaivel S G <thanigaigopal@outlook.com>
2022-10-09 22:29:01 +0530

    Updated with Deployment code

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,9 @@
 Database Models
 """
 
+import uuid
+import os
+
 from django.conf import settings
 
 from django.db import models
@@ -10,6 +13,15 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for a new recipe image"""
+
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename   )
 
 class UserManager(BaseUserManager):
     """Manager for users"""
@@ -36,6 +48,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
 
@@ -47,6 +60,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
 
 class Recipe(models.Model):
     """Recipe objects."""
@@ -60,6 +75,35 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits = 5, decimal_places = 2)
     link = models.CharField(max_length = 255, blank = True)
+    tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes"""
+
+    name = models.CharField(max_length = 255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for filtering recipes"""
+
+    name = models.CharField(max_length = 255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
